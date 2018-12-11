e281773fac63e1f61badce4429c28ae673e9f440
Lan Anh <s0554822@htw-berlin.de>
2018-12-11 11:54:28 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- Test191118/TableViewController.swift
+++ Test191118/TableViewController.swift
@@ -11,22 +11,31 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    let headlines = ["Apple Service","Amazon Web Service"]
     
-    let headlines = ["Apple Services","Amazon Web Services"]
+    //    let status = [["App Store", "Device Enrollment Programm", "iOS Device Activation", "Mac App Store", "macOS Software Update", "Volume Purchase Program"],
+    //                  ["Asia Pacific", "Europe", "North America", "South America" ]]
+    var services: [Service] = []
     
-    let status = [["App Store", "Device Enrollment Programm", "iOS Device Activation", "Mac App Store", "macOS Software Update", "Volume Purchase Program"],
-                  ["Asia Pacific", "Europe", "North America", "South America" ]]
+    var appleServicesParser: Parser!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        appleServicesParser = Parser(url: URL(string:"https://www.apple.com/support/systemstatus/")!) { [weak self] services in
+            self?.services = services
+            self?.tableView.reloadData()
+        }
+        appleServicesParser.parse()
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return status.count
-    }
+    
+    //    override func numberOfSections(in tableView: UITableView) -> Int {
+    //        // #warning Incomplete implementation, return the number of sections
+    //        return headlines.count
+    //    }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return headlines[section]
@@ -34,17 +43,17 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return status[section].count
+        return services.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCellApple", for: indexPath)
-
-        cell.textLabel?.text = status[indexPath.section][indexPath.row]
-
+        let service = services[indexPath.row]
+        cell.textLabel?.text = service.name
+        cell.detailTextLabel?.text = service.status
         return cell
     }
     
     
-
+    
 }
